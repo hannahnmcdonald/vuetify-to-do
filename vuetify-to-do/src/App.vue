4b8c69c898c657ba9b0158e7b3d02b16510c78a9
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-02-16 18:47:29 -0500

    mobile responsive search component and split menu options and title up into separate v-row

--- vuetify-to-do/src/App.vue
+++ vuetify-to-do/src/App.vue
@@ -46,27 +46,19 @@
         ></v-img>
       </template>
 
-      <v-app-bar-nav-icon
-        @click="drawer = !drawer"
-      ></v-app-bar-nav-icon>
+            <v-container class="pa-0">
+        <v-row>
+          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-spacer></v-spacer>
+          <search />
+        </v-row>
+        <v-row>
+          <v-toolbar-title class="ml-4">Vuetify Todo</v-toolbar-title>
+        </v-row>
+      </v-container>
 
-      <v-app-bar-title></v-app-bar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+      </v-app-bar>
+      
     <v-main>
       <router-view></router-view>
     </v-main>
