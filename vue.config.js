cceab6935bdf25baf2b248c49d66ce264a23eda7
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-03-25 18:10:21 -0400

    deploy sh & vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
-var path = require('path');
-var webpack = require('webpack');
-
 module.exports = {
-  pages: {
-    index: {
-      entry: './vuetify-to-do/src/main.ts'
-    }
-  }
+  publicPath: '/vue-app-deploy-example/'
 }
